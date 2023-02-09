aeca95bf18dfb393df531f63331f680a9cd742af
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-02-09 14:00:46 +0700

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,8 @@
                     <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2"
                             aria-hidden="true"></i></div>
                 </li>
-                <li><a class="nav-link menu-title link-nav" href="{{ route('dashboard') }}"> <span>Dashboard</span></a>
+                <li><a class="nav-link menu-title link-nav" href="{{ route('dashboard') }}"><i data-feather="home"></i>
+                        <span>Dashboard</span></a>
                 </li>
                 @foreach (config('generator.sidebars') as $sidebar)
                     @if (isset($sidebar['permissions']))
