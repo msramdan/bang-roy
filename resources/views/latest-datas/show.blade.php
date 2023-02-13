6615a762795ceeb7174a17249e7eb3f6d1b03d5f
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-02-13 13:30:19 +0700

    update

--- resources/views/latest-datas/show.blade.php
+++ resources/views/latest-datas/show.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('title', __('Detail of Latest Datas'))
+@push('css')
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.25/daterangepicker.min.css">
+@endpush
 
 @section('content')
     <style>
@@ -82,6 +92,41 @@
             </div>
         </div>
         <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <!-- Date and time range -->
+                                    <div class="form-group">
+                                        <label>Filter Date :</label>
+
+                                        <div class="input-group-btn">
+                                            <button type="button" class="btn btn-light" id="daterange-btn"
+                                                style='width:230px'>
+
+                                                <i class="fa fa-calendar"></i>&nbsp; <span>defaut date</span>
+
+                                                <i class="fa fa-caret-down"></i>
+                                            </button>
+                                            <button id='btnDec' type="button" class="btn btn-danger btn-flat"
+                                                title='Decrement month'><i class="fa fa-calendar-minus-o"
+                                                    aria-hidden="true"></i></button>
+                                            <button id='btnInc' type="button" class="btn btn-info btn-flat"
+                                                title='Increment month'><i class="fa fa-calendar-plus-o"
+                                                    aria-hidden="true"></i></button>
+
+                                        </div>
+                                    </div>
+                                    <!-- /.form group -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-md-5">
                     <div class="card">
@@ -199,3 +244,75 @@
         </div>
     </div>
 @endsection
+@push('js')
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.js"></script>
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/2.1.25/daterangepicker.min.js"></script>
+    <script>
+        //var start = ''; //moment().startOf('month');
+        //var end = ''; //moment().endOf('month');
+        //var label = '';
+
+        var start = moment().startOf('month');
+        var end = moment().endOf('month');
+        var label = '';
+
+        $('#daterange-btn').daterangepicker({
+                locale: {
+                    format: 'DD MMM YYYY'
+                },
+                startDate: moment(start),
+                endDate: moment(end),
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf(
+                        'month')],
+                }
+            },
+            function(start, end, label) {
+                if (isDate(start)) {
+                    $('#daterange-btn span').html(start.format('DD MMM YYYY') + ' - ' + end.format('DD MMM YYYY'));
+                }
+            });
+
+        $('#btnInc').click(function(e) {
+            IncDecMonth('Inc')
+        })
+
+        $('#btnDec').click(function(e) {
+            IncDecMonth('Dec')
+        })
+
+        function isDate(val) {
+            //var d = new Date(val);
+            //return !isNaN(d.valueOf());
+            var d = Date.parse(val);
+            console.log(d);
+            return Date.parse(val);
+        }
+
+        function IncDecMonth(Action) {
+            if (!isDate(start)) {
+                start = moment().startOf('month');
+            }
+            if (Action == 'Inc') {
+                start = moment(start).add(1, 'month').startOf('month');
+                end = moment(start).endOf('month')
+            } else {
+                start = moment(start).subtract(1, 'month').startOf('month');
+                end = moment(start).endOf('month')
+            }
+            if (isDate(start)) {
+                $('#daterange-btn span').html(start.format('DD MMM YYYY') + ' - ' + end.format('DD MMM YYYY'));
+            }
+            console.log('start=' + start)
+            console.log('end=' + end)
+        }
+
+        IncDecMonth();
+    </script>
+@endpush
