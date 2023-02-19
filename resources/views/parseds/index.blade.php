55cc056abb732b6840c126e37700ab5daf7a2789
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-02-19 19:21:23 +0700

    update

--- resources/views/parseds/index.blade.php
+++ resources/views/parseds/index.blade.php
@@ -31,9 +31,43 @@
                             </a>&nbsp;
                         @endif
                         <a href="" class="btn btn-primary mb-3">
-                            <i class="fa fa-upload" aria-hidden="true"></i>
-                            {{ __('Upload Parsed Data') }}
-                        </a>
+                            <i class="fas fa-download"></i>
+                            {{ __('Download Format') }}
+                        </a>&nbsp;
+
+                        <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">
+                            <i class="fa fa-upload" aria-hidden="true"></i> {{ __('Upload Parsed Data') }}
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <form action="" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Upload Parsed Data</h5>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="mb-3">
+                                                <label for="dev_eui" class="form-label">File Excel</label>
+                                                <input type="file" id="dev_eui" name="dev_eui" class="form-control"
+                                                    value="" placeholder="">
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Upload</button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+
                     </div>
                     <div class="card">
                         <div class="card-body">
