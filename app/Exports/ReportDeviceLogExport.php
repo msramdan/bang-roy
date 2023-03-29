f176e8a9172279b46cc3e4926761aaac40bbafdf
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-03-29 07:06:19 +0700

    update fix error export and filter report

--- app/Exports/ReportDeviceLogExport.php
+++ app/Exports/ReportDeviceLogExport.php
@@ -10,10 +10,11 @@ use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
 
-class ReportDeviceLogExport implements FromView,ShouldAutoSize, WithEvents
+class ReportDeviceLogExport implements FromView, ShouldAutoSize, WithEvents
 {
-    function __construct($dev_eui, $start_date, $end_date) {
-        $this->dev_eui = intval($dev_eui);
+    function __construct($dev_eui, $start_date, $end_date)
+    {
+        $this->dev_eui = $dev_eui;
         $this->start_date = intval($start_date);
         $this->end_date = intval($end_date);
     }
@@ -21,19 +22,19 @@ class ReportDeviceLogExport implements FromView,ShouldAutoSize, WithEvents
     {
         $rawdatas = Rawdata::query();
         if (isset($this->dev_eui) && !empty($this->dev_eui)) {
-            if($this->dev_eui !='All'){
+            if ($this->dev_eui != 'All') {
                 $rawdatas = $rawdatas->where('dev_eui', $this->dev_eui);
             }
         }
 
         if (isset($this->start_date) && !empty($this->start_date)) {
-                $from = date("Y-m-d H:i:s", substr($this->start_date, 0, 10));
-                $rawdatas = $rawdatas->where('created_at', '>=', $from);
+            $from = date("Y-m-d H:i:s", substr($this->start_date, 0, 10));
+            $rawdatas = $rawdatas->where('created_at', '>=', $from);
         }
 
         if (isset($this->end_date) && !empty($this->end_date)) {
-                $to = date("Y-m-d H:i:s", substr($this->end_date, 0, 10));
-                $rawdatas = $rawdatas->where('created_at', '<=', $to);
+            $to = date("Y-m-d H:i:s", substr($this->end_date, 0, 10));
+            $rawdatas = $rawdatas->where('created_at', '<=', $to);
         }
         $rawdatas = $rawdatas->orderBy('rawdatas.id', 'desc')->get();
         return view('report-devices.export', [
@@ -44,16 +45,16 @@ class ReportDeviceLogExport implements FromView,ShouldAutoSize, WithEvents
     public function registerEvents(): array
     {
         return [
-            AfterSheet::class    => function(AfterSheet $event) {
+            AfterSheet::class    => function (AfterSheet $event) {
                 $cellRange = 'A1:O1'; // All headers
-                $event->sheet->getStyle( $cellRange)->applyFromArray([
+                $event->sheet->getStyle($cellRange)->applyFromArray([
                     'borders' => [
                         'allBorders' => [
                             'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
                             'color' => ['argb' => '000000'],
                         ],
                     ],
-         ]);
+                ]);
             },
         ];
     }
--- app/Exports/ReportGatewayLogExport.php
+++ app/Exports/ReportGatewayLogExport.php
@@ -10,10 +10,11 @@ use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
 
-class ReportGatewayLogExport implements FromView,ShouldAutoSize, WithEvents
+class ReportGatewayLogExport implements FromView, ShouldAutoSize, WithEvents
 {
-    function __construct($gwid, $start_date, $end_date) {
-        $this->gwid = intval($gwid);
+    function __construct($gwid, $start_date, $end_date)
+    {
+        $this->gwid = $gwid;
         $this->start_date = intval($start_date);
         $this->end_date = intval($end_date);
     }
@@ -21,19 +22,19 @@ class ReportGatewayLogExport implements FromView,ShouldAutoSize, WithEvents
     {
         $rawdatas = GatewayLog::query();
         if (isset($this->gwid) && !empty($this->gwid)) {
-            if($this->gwid !='All'){
+            if ($this->gwid != 'All') {
                 $rawdatas = $rawdatas->where('gateway_id', $this->gwid);
             }
         }
 
         if (isset($this->start_date) && !empty($this->start_date)) {
-                $from = date("Y-m-d H:i:s", substr($this->start_date, 0, 10));
-                $rawdatas = $rawdatas->where('created_at', '>=', $from);
+            $from = date("Y-m-d H:i:s", substr($this->start_date, 0, 10));
+            $rawdatas = $rawdatas->where('created_at', '>=', $from);
         }
 
         if (isset($this->end_date) && !empty($this->end_date)) {
-                $to = date("Y-m-d H:i:s", substr($this->end_date, 0, 10));
-                $rawdatas = $rawdatas->where('created_at', '<=', $to);
+            $to = date("Y-m-d H:i:s", substr($this->end_date, 0, 10));
+            $rawdatas = $rawdatas->where('created_at', '<=', $to);
         }
         $rawdatas = $rawdatas->orderBy('id', 'desc')->get();
         return view('report-gateways.export', [
@@ -44,16 +45,16 @@ class ReportGatewayLogExport implements FromView,ShouldAutoSize, WithEvents
     public function registerEvents(): array
     {
         return [
-            AfterSheet::class    => function(AfterSheet $event) {
+            AfterSheet::class    => function (AfterSheet $event) {
                 $cellRange = 'A1:E1'; // All headers
-                $event->sheet->getStyle( $cellRange)->applyFromArray([
+                $event->sheet->getStyle($cellRange)->applyFromArray([
                     'borders' => [
                         'allBorders' => [
                             'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
                             'color' => ['argb' => '000000'],
                         ],
                     ],
-         ]);
+                ]);
             },
         ];
     }
