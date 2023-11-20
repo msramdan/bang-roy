123c1cccf03b03c88699e08cd0556a4f3063a6f5
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-11-20 20:22:03 +0700

    update

--- database/seeders/VisiMisiSeeder.php
+++ database/seeders/VisiMisiSeeder.php
@@ -11,15 +11,8 @@ class VisiMisiSeeder extends Seeder
     public function run()
     {
         Vm::create([
-            'visi' => 'Being a reliable and committed company
-            by offering products and services with
-            quality service to consumers in
-            Industrial appliances and Engineering.',
-            'misi' => 'As a company that is reliable in its field by
-            offering quality products and services and is
-            able to increase human resources and can
-            support national economic development by
-            providing professional services',
+            'visi' => 'Being a reliable and committed company by offering products and services with quality service to consumers in Industrial appliances and Engineering.',
+            'misi' => 'As a company that is reliable in its field by offering quality products and services and is able to increase human resources and can support national economic development by providing professional services',
         ]);
     }
 }
--- resources/views/vms/include/form.blade.php
+++ resources/views/vms/include/form.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-6">
         <div class="form-group">
             <label for="visi">{{ __('Visi') }}</label>
-            <textarea name="visi" id="visi" class="form-control @error('visi') is-invalid @enderror" placeholder="{{ __('Visi') }}" required>{{ isset($vm) ? $vm->visi : old('visi') }}</textarea>
+            <textarea name="visi" rows="5" id="visi" class="form-control  @error('visi') is-invalid @enderror" placeholder="{{ __('Visi') }}" required>{{ isset($vm) ? $vm->visi : old('visi') }}</textarea>
             @error('visi')
                 <span class="text-danger">
                     {{ $message }}
@@ -13,7 +13,7 @@
     <div class="col-md-6">
         <div class="form-group">
             <label for="misi">{{ __('Misi') }}</label>
-            <textarea name="misi" id="misi" class="form-control @error('misi') is-invalid @enderror" placeholder="{{ __('Misi') }}" required>{{ isset($vm) ? $vm->misi : old('misi') }}</textarea>
+            <textarea name="misi" rows="5" id="misi" class="form-control @error('misi') is-invalid @enderror" placeholder="{{ __('Misi') }}" required>{{ isset($vm) ? $vm->misi : old('misi') }}</textarea>
             @error('misi')
                 <span class="text-danger">
                     {{ $message }}
