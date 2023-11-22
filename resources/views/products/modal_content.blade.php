6a3f528d26aa19f11f854232d90258b29839d260
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-11-22 12:53:08 +0700

    update

--- resources/views/products/modal_content.blade.php
+++ resources/views/products/modal_content.blade.php
@@ -3,7 +3,10 @@
 <div class="row">
     <div class="col-lg-6 col-xs-12">
         <div class="quick-view-img">
-            <img src="{{ asset('storage/uploads/photos/' . $product->photo) }}" alt="" class="img-fluid">
+            <center>
+                <img src="{{ asset('storage/uploads/photos/' . $product->photo) }}" alt="" class="img-fluid">
+            </center>
+
         </div>
     </div>
     <div class="col-lg-6 rtl-text">
@@ -15,7 +18,8 @@
                 <p>{{ $product->deskripsi }}</p>
             </div>
             <div class="product-buttons">
-                <a href="https://wa.me/{{ setting_web()->no_whatsapp }}?text=Hello%20Admin,%0ASaya%20ingin%20bertanya%20terkait%20product%20{{ urlencode($product->nama) }}" class="btn btn-solid" style="color: white" target="_blank">Order Via WhatsApp</a>
+                <a href="https://wa.me/{{ setting_web()->no_whatsapp }}?text=Hello%20Admin,%0ASaya%20ingin%20bertanya%20terkait%20product%20{{ urlencode($product->nama) }}"
+                    class="btn btn-solid" style="color: white" target="_blank">Order Via WhatsApp</a>
             </div>
         </div>
     </div>
